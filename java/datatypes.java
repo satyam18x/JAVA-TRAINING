0696238d949508507f4e864b4a8ed3a411fe9be2
unknown <satyamhaldkar45@gmail.com>
2025-04-09 10:50:24 +0530

    satyam

---
+++ java/datatypes.java
@@ -0,0 +1,28 @@
+public class datatypes {
+    public static void main(String[] args)
+    {
+        byte a=10;
+        System.out.println(a);
+
+        char z='a';
+        System.out.println(z);
+
+        short b=10;
+        System.out.println(b);
+
+        int c=120;
+        System.out.println(c);
+
+        long d=107;
+        System.out.println(d);
+
+        double e=1273.8;
+        System.out.println(e);
+
+        float x=1072.76f;
+        System.out.println(x);
+
+        boolean g=true;
+        System.out.println(g);
+    }
+}
---
+++ java/swapping2.java
@@ -0,0 +1,14 @@
+public class swapping2 {
+    public static void main(String[] args)
+    {
+        int q= 5;
+        int r= 10;
+        
+        int temp=q;
+        q=r;
+        r=temp;
+        System.out.println("q="+q);
+         System.out.println("r="+r);
+        
+    }
+}
