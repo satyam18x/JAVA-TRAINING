ad667bed54ae0415556abbfad4be078523a76f78
unknown <satyamhaldkar45@gmail.com>
2025-04-25 12:42:37 +0530

    25 april

---
+++ java/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+
+    int a=10; // instance variable
+    static int b=20; // static variable
+    public static void main(String[] args){
+       int c=10;
+        System.out.println(a); // error due to instance varibale
+        System.out.println(b);
+        System.out.println(c); // local variable
+    
+        
+    }
+}
+
