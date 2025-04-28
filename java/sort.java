2776687678812c37b68a4ffe9253893bb501c261
unknown <satyamhaldkar45@gmail.com>
2025-04-28 10:38:51 +0530

    28 april

---
+++ java/sort.java
@@ -0,0 +1,27 @@
+public class sort {
+
+      public static  void bubblesort(int[] a){
+
+        for(int i=0;i<a.length-1;i++){
+            for (int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1])
+                {
+                   int temp=a[j];
+                   a[j]=a[j+1];
+                   a[j+1]=temp;
+
+                }
+            }
+        }
+
+       
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+       
+
+    }
+    public static void main(String[] args){
+        int[] a={22,34,12,3,56,87,2,3};
+        bubblesort(a);
+    }
+}
