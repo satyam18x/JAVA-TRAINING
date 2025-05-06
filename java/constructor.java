399ba046c59eeff8f670532f09fc03040656ed22
unknown <satyamhaldkar45@gmail.com>
2025-05-06 13:46:35 +0530

    6 may

---
+++ java/constructor.java
@@ -0,0 +1,23 @@
+class student {
+    String name;
+    int age;
+
+    student() {
+        name = "John"; 
+        age = 20;      
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        student obj = new student(); 
+    }
+}
+
+       
+        
+
+
+       
+
