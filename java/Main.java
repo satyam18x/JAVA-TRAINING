a19ef1077c60dfe7f8c564aa9025d02ae1ca2d80
unknown <satyamhaldkar45@gmail.com>
2025-06-03 09:55:19 +0530

    h

--- java/Main.java
+++ java/Main.java
@@ -15,13 +15,34 @@ class rectangle{
         
     }
 }
+
+class student {
+    private String name;
+    private int marks;
+public void setinfo(String name,int marks){
+ this.name=name;
+ this.marks=marks;
+}
+
+public String getname(){
+ return name;
+ }
+ public int getmarks(){
+    return marks;
+    }
+}
+
 class Main{
     public static void main(String[] args){
       rectangle obj=new rectangle();
       obj.setfields(20,30);
       System.out.println( obj.getareafields());
-    
+    student ob = new student();
+    ob.setinfo("satyam",99);
+    System.out.println(ob.getname());
+
     
     }
 }
+
 
